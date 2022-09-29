6ef2bf7c71148cadbab6e787128a88835587b477
Wandir Pereira Filho <wandir.filho@csn.com.br>
2022-09-29 15:19:12 -0300

    Commit inicial

---
+++ ByteBank.Portal/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ByteBank.Portal.Infraestrutura;
+
+//Console.WriteLine("Hello, World!");
+
+var prefixos = new string[] { "http://localhost:5341/" };
+var webApplication = new WebApplication(prefixos);
+webApplication.Iniciar();
